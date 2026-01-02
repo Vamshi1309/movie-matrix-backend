3cb2850b0e19c2deaefd4f8d740c538912b2d804
Vamshi1309 <vamshidasari08@gmail.com>
2026-01-02 18:32:38 +0530

    intial commits

---
+++ src/main/java/com/backend/movie_matrix/MovieMatrixApplication.java
@@ -0,0 +1,13 @@
+package com.backend.movie_matrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieMatrixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieMatrixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/movie_matrix/MovieMatrixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.movie_matrix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieMatrixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
